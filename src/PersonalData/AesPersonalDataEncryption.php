00de76db397275710ee0be9aa05047ad9bb66111
Shawn McCool <shawn@heybigname.com>
2018-04-20 15:41:25 +0200

    make a thing readable

--- src/PersonalData/AesPersonalDataEncryption.php
+++ src/PersonalData/AesPersonalDataEncryption.php
@@ -4,13 +4,27 @@ class AesPersonalDataEncryption implements PersonalDataEncryption {
 
     function encrypt(CryptographicDetails $crypto, PersonalData $data): EncryptedPersonalData {
         return EncryptedPersonalData::fromString(
-            base64_encode(openssl_encrypt($data->toString(), 'aes-256-cbc', $crypto->key()->toString(), OPENSSL_RAW_DATA, $crypto->iv()->toBinary()))
+            base64_encode(
+                openssl_encrypt(
+                    $data->toString(),
+                    'aes-256-cbc',
+                    $crypto->key()->toString(),
+                    OPENSSL_RAW_DATA,
+                    $crypto->iv()->toBinary()
+                )
+            )
         );
     }
 
     function decrypt(CryptographicDetails $crypto, EncryptedPersonalData $data): PersonalData {
         return PersonalData::fromString(
-            openssl_decrypt(base64_decode($data->toString()), 'aes-256-cbc', $crypto->key()->toString(), OPENSSL_RAW_DATA, $crypto->iv()->toBinary())
+            openssl_decrypt(
+                base64_decode($data->toString()),
+                'aes-256-cbc',
+                $crypto->key()->toString(),
+                OPENSSL_RAW_DATA,
+                $crypto->iv()->toBinary()
+            )
         );
     }
 }
