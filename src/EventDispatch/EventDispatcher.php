2a89515aac494a8338cddfad108027bd648f321b
Shawn McCool <shawn@heybigname.com>
2018-04-20 15:51:24 +0200

    remove log domain events listener

--- src/EventDispatch/EventDispatcher.php
+++ src/EventDispatch/EventDispatcher.php
@@ -5,6 +5,5 @@ use EventSourcery\EventSourcing\DomainEvents;
 interface EventDispatcher {
 
     public function addListener(Listener $listener): void;
-
     public function dispatch(DomainEvents $events): void;
 }
