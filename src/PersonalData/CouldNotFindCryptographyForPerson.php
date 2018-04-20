5fd2c3ee09a9c25d8da8d42e0eb3ca278223fc13
Shawn McCool <shawn@heybigname.com>
2018-04-20 15:20:02 +0200

    rename

---
+++ src/PersonalData/CouldNotFindCryptographyForPerson.php
@@ -0,0 +1,3 @@
+<?php namespace EventSourcery\PersonalData;
+
+class CouldNotFindCryptographyForPerson extends \Exception {}
--- src/PersonalData/CouldNotFindEncryptionKeyForPerson.php
+++
@@ -1,3 +0,0 @@
-<?php namespace EventSourcery\PersonalData;
-
-class CouldNotFindEncryptionKeyForPerson extends \Exception {}
