b939b8c2002dfec32f6472323d2012f545aad5e3
Shawn McCool <shawn@heybigname.com>
2018-06-26 13:00:56 +0200

    cleanup testing prototype a bit

--- spec/PhpSpec/TestingToolsSpec.php
+++ spec/PhpSpec/TestingToolsSpec.php
@@ -1,7 +1,6 @@
 <?php namespace spec\EventSourcery\EventSourcery\PhpSpec;
 
 use EventSourcery\EventSourcery\EventSourcing\StreamId;
-use spec\EventSourcery\EventSourcery\Commands\ArgumentCommandStub;
 use spec\EventSourcery\EventSourcery\Commands\ResolutionTargetStub;
 use spec\EventSourcery\EventSourcery\Commands\Stubs\TestCountingCommand;
 use spec\EventSourcery\EventSourcery\EventSourcing\TestCountingEvent;
@@ -27,69 +26,3 @@ class TestingToolsSpec extends EventSourcedBehavior {
         );
     }
 }
-
-class Test {
-
-
-}
-
-/**
-
- public class AggregateSpec {
-
-readonly CommandBus Bus;
-readonly EventStream Events;
-Command Command;
-
-List<DomainEvent> ReceivedEvents { get; set; }
-
-public AggregateSpec(CommandBus bus, EventStream events) {
-    Bus = bus;
-    Events = events;
-    ReceivedEvents = new List<DomainEvent>();
-
-            events.DomainEventReceived += ev => ReceivedEvents.Add(ev);
-        }
-
-        public AggregateSpec given() {
-            return this;
-        }
-
-        public AggregateSpec given(Id streamId, params DomainEvent[] history) {
-    Events.Publish(streamId, new List<DomainEvent>(history));
-            return this;
-        }
-
-        public AggregateSpec when(Command command) {
-    Command = command;
-    return this;
-}
-
-        public void then(params DomainEvent[] domainEvents) {
-    Bus.Handle(Command);
-
-    foreach (var domainEvent in domainEvents) {
-        var found = false;
-        foreach (var receivedEvent in ReceivedEvents) {
-
-            if (domainEvent.GetType().ToString().Equals(receivedEvent.GetType().ToString())) {
-                if (JsonConvert.SerializeObject(domainEvent) == JsonConvert.SerializeObject(receivedEvent)) {
-                    found = true;
-                }
-            }
-        }
-
-                if (! found) {
-                    Assert.Fail("Could not find " + domainEvent.GetType().ToString() + " " + JsonConvert.SerializeObject(domainEvent) + " in received events.");
-                }
-            }
-        }
-
-        public void thenFail<T>() where T : Exception {
-    Assert.Throws<T>(
-    delegate { Bus.Handle(Command); } );
-
-        }
-
-    }
- **/
